fcacdd08ea2a10877ea29267c577cf16d8f7196a
George Ramirez <george.ramirez2@gmail.com>
2016-08-29 14:57:16 -0500

    got all the correct clicks to go through the whole game

--- assets/javascript/trivia.js
+++ assets/javascript/trivia.js
@@ -57,28 +57,30 @@ $(document).ready(function(){
 		$(".answer2").html("Q1 Answer 2");
 
 		$(".answer3").html("Q1 Answer 3, this is the correct answer");
-		$(".answer3").on("click",this.question2);
+		$(".answer3").on("click",trivia.question2.bind(this));
 
 		$(".answer4").html("Q1 Answer 4");
-console.log(this);
+		
 		},/*end of question1*/
 	
 	
 	question2:function(){
 
 		/*.off or unbind click lilstener*/
+		 
+		 $(".answer3").off("click");
 
-		$(".image").hide();
-		$(".question").show();
-		$(".answer1").show();
-		$(".answer2").show();
-		$(".answer3").show();
-		$(".answer4").show();
-		$(".correct").hide();
-		$(".incorrect").hide();
-		$(".outOfTime").hide();
-		$("#timermsg").show();
-		$(".startB").hide();
+		// $(".image").hide();
+		// $(".question").show();
+		// $(".answer1").show();
+		// $(".answer2").show();
+		// $(".answer3").show();
+		// $(".answer4").show();
+		// $(".correct").hide();
+		// $(".incorrect").hide();
+		// $(".outOfTime").hide();
+		// $("#timermsg").show();
+		// $(".startB").hide();
 
 		$(".question").html("QUESTION TWO TEXT");
 
@@ -90,22 +92,194 @@ console.log(this);
 		$(".answer3").html("Q2 Answer 3");
 		
 
-		$(".answer4").html("Q2 Answer 4");
-		$(".answer4").on("click", this.question3);
+		$(".answer4").html("Q2 Answer 4***");
+		$(".answer4").on("click", trivia.question3);
 
-		},/*endo of quesiton 2*/
+		},/*endo of quesiton 3*/
 
 	question3:function(){
-		console.log("on to question3")
 
-	}
-	// question4:function()
-	// question5:function()
-	// question6:function()
-	// question7:function()
-	// question8:function()
-	// question9:function()
-	// question10:function()
+		$(".answer4").off("click");
+
+		
+		$(".question").html("QUESTION THREE TEXT");
+
+
+		$(".answer1").html("Q3 Answer 1**");
+		$(".answer1").on("click", trivia.question4);
+
+		$(".answer2").html("Q3 Answer 2");
+
+		$(".answer3").html("Q3 Answer 3");
+		
+
+		$(".answer4").html("Q3 Answer 4");
+		
+		
+	},/*End of Question3*/
+
+
+	question4:function(){
+
+		$(".answer1").off("click");
+
+		
+
+		$(".question").html("QUESTION FOUR TEXT");
+
+
+		$(".answer1").html("Q4 Answer 1");
+	
+
+		$(".answer2").html("Q4 Answer 2");
+
+		$(".answer3").html("Q4 Answer 3**");
+		$(".answer3").on("click", trivia.question5);
+
+		$(".answer4").html("Q4 Answer 4");
+		console.log(trivia);
+
+	},/*End of question4*/
+	
+
+	question5:function(){
+
+		$(".answer3").off("click");
+
+		
+
+		$(".question").html("QUESTION FIVE TEXT");
+
+
+		$(".answer1").html("Q5 Answer 1");
+		
+
+		$(".answer2").html("Q5 Answer 2**");
+		$(".answer2").on("click", trivia.question6);
+
+		$(".answer3").html("Q5 Answer 3");
+	
+
+		$(".answer4").html("Q5 Answer 4");
+
+	},/*End of question5*/
+
+	question6:function(){
+
+		$(".answer2").off("click");
+
+		
+
+		$(".question").html("QUESTION SIX TEXT");
+
+
+		$(".answer1").html("Q6 Answer 1");
+		
+
+		$(".answer2").html("Q6 Answer 2");
+		
+
+		$(".answer3").html("Q6 Answer 3");
+	
+
+		$(".answer4").html("Q6 Answer 4**");
+		$(".answer4").on("click", trivia.question7);
+
+	},/*End of question6*/
+
+	question7:function(){
+
+		$(".answer4").off("click");
+
+		
+
+		$(".question").html("QUESTION SEVEN TEXT");
+
+
+		$(".answer1").html("Q7 Answer 1");
+		
+
+		$(".answer2").html("Q7 Answer 2");
+		
+
+		$(".answer3").html("Q7 Answer 3");
+	
+
+		$(".answer4").html("Q7 Answer 4**");
+		$(".answer4").on("click", trivia.question8);		
+
+	},/*End of question7*/
+
+
+	question8:function(){
+
+		$(".answer4").off("click");
+
+
+
+		$(".question").html("QUESTION EIGHT TEXT");
+
+
+		$(".answer1").html("Q8 Answer 1");
+		
+
+		$(".answer2").html("Q8 Answer 2**");
+		$(".answer2").on("click", trivia.question9);
+
+		$(".answer3").html("Q8 Answer 3");
+	
+
+		$(".answer4").html("Q8 Answer 4");
+		
+
+	},/*END OF QUESTION 8*/
+
+
+	question9:function(){
+
+		$(".answer2").off("click");
+
+		
+
+		$(".question").html("QUESTION NINE TEXT");
+
+
+		$(".answer1").html("Q89 Answer 1**");
+		$(".answer1").on("click", trivia.question10);
+
+		$(".answer2").html("Q9 Answer 2");
+		
+
+		$(".answer3").html("Q9 Answer 3");
+	
+
+		$(".answer4").html("Q9 Answer 4");
+
+	},/*END OF QUESITON 9*/
+
+
+	question10:function(){
+
+		$(".answer1").off("click");
+
+
+		$(".question").html("QUESTION TEN TEXT");
+
+
+		$(".answer1").html("Q10 Answer 1");
+		
+
+		$(".answer2").html("Q10 Answer 2");
+		
+
+		$(".answer3").html("Q10 Answer 3");
+	
+
+		$(".answer4").html("Q10 Answer 4**");
+		$(".answer4").on("click", trivia.question1);
+
+	}/*End of question 10*/
+
 	// score:function()
 		
 
